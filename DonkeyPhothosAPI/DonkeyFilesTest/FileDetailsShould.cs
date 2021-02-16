87f3cb535a6c4829b0f9dee2afba93f47e56dd27
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2021-02-16 17:01:20 +0300

    Test

---
+++ DonkeyPhothosAPI/DonkeyFilesTest/FileDetailsShould.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using DonkeyFilesBL.Interfaces;
+using DonkeyFilesBL.Files;
+using DonkeyFilesDAL.Models;
+
+namespace DonkeyFilesTest
+{
+    public class FileDetailsShould
+    {
+        [Fact]
+        public void ValidateFileDetailsExists()
+        {
+            //Arrange
+            var helper = new TestHelper();
+            var repo = helper.GetInMemoryReadRepository();
+            repo.PostFileDetails(new FileDetailsModel {FileId =1, FileDate= string.Empty, FileName=string.Empty, FileSize= string.Empty });
+            int fileId = 1;
+
+            //Act
+            bool isValid = repo.FileDetailsExists(fileId);
+
+            //Assert
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void ValidateFileDetailsNoExists()
+        {
+            //Arrange
+            var helper = new TestHelper();
+            var repo = helper.GetInMemoryReadRepository();
+            int fileId = 1;
+
+            //Act
+            bool isValid = repo.FileDetailsExists(fileId);
+
+            //Assert
+            Assert.False(isValid);
+        }
+    }
+}
---
+++ DonkeyPhothosAPI/DonkeyFilesTest/TestHelper.cs
@@ -0,0 +1,33 @@
+﻿using DonkeyFilesDAL.Context;
+using DonkeyFilesDAL.Interface;
+using DonkeyFilesDAL.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonkeyFilesTest
+{
+   public class TestHelper
+    {
+        private readonly FilesContext fileContext;
+        public TestHelper()
+        {
+            var builder = new DbContextOptionsBuilder<FilesContext>();
+            builder.UseInMemoryDatabase(databaseName: "LibraryDbInMemory");
+
+            var dbContextOptions = builder.Options;
+            fileContext = new FilesContext(dbContextOptions);
+            // Delete existing db before creating a new one
+            fileContext.Database.EnsureDeleted();
+            fileContext.Database.EnsureCreated();
+        }
+
+        public IFileDetailsRepository GetInMemoryReadRepository()
+        {
+            return new FileDetailsRepository(fileContext);
+        }
+    }
+}
